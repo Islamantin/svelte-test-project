9d248b05c0c07ec61b8d211968b0cce928748e49
Islamantin <islamantin@gmail.com>
2023-08-19 13:46:24 +0900

    got rid of commented code

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,13 +1,7 @@
 <script>
-//   import { onDestroy } from "svelte";
   import { FeedbackStore } from "../stores";
-  import { fade, scale } from "svelte/transition";
+  import { scale } from "svelte/transition";
   import FeedbackItem from "./FeedbackItem.svelte";
-//   let feedback = [];
-
-//   const unsubscribe = FeedbackStore.subscribe((data) => (feedback = data));
-
-//   onDestroy(unsubscribe);
 </script>
 
 {#each $FeedbackStore as fb (fb.id)}
